771dc89e12281c5b80b9a28a4de40c7039300f5d
15716075014 <1742192346@qq.com>
2025-07-31 09:44:47 +0800

    项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import gsap from "gsap"
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = ()=> {
+  return (
+    <div>
+      <h1>Hello Vite</h1>
+    </div>
+  )
+}
+
+export default App
